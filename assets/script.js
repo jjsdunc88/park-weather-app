7b3793f1b67e36dcfdde977d7e85b3344703544c
JDunc <jjsduncan@gmail.com>
2023-04-27 19:31:43 -0400

    update getBeer API call to search by city

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,14 @@
 
 console.log("howdy");
 
+
 // National Park Service API Key: 8fzgFBy23bOctVVOvssIxHSKu8vDZPKnUKcTNKfM
 
-document.querySelector("#applyButton").addEventListener("click", getCity);
+// document.querySelector("#applyButton").addEventListener("click", getCity);
 
-// document.querySelector("#applyButton").addEventListener("click", getBeer);
-
-function getCity() {
-    var searchBar = document.querySelector("#searchBar").value;
-    var url = "https://developer.nps.gov/api/v1/parks?q=" + searchBar + "&api_key=8fzgFBy23bOctVVOvssIxHSKu8vDZPKnUKcTNKfM"
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-};
-
-const resetButton = document.querySelector("#resetButton");
-resetButton.addEventListener("click", function () {
-    const inputField = document.querySelector(".input");
-    inputField.value = "";
-})
-
-
-
-// Beer Mapping API Key: f82c35d11ba2db8be445456f47487b12
-
-
-
-// function getBeer() {
+// function getCity() {
 //     var searchBar = document.querySelector("#searchBar").value;
-//     var url = "http://beermapping.com/webservice/locquery/f82c35d11ba2db8be445456f47487b12/" + searchBar + "&s=json"
+//     var url = "https://developer.nps.gov/api/v1/parks?q=" + searchBar + "&api_key=8fzgFBy23bOctVVOvssIxHSKu8vDZPKnUKcTNKfM"
 //     fetch(url)
 //         .then(function (response) {
 //             return response.json()
@@ -42,3 +17,29 @@ resetButton.addEventListener("click", function () {
 //             console.log(data);
 //         })
 // };
+
+// const resetButton = document.querySelector("#resetButton");
+// resetButton.addEventListener("click", function () {
+//     const inputField = document.querySelector(".input");
+//     inputField.value = "";
+// })
+
+
+
+
+
+// Beer Mapping API Key: f82c35d11ba2db8be445456f47487b12
+
+document.querySelector("#applyButton").addEventListener("click", getBeer);
+
+function getBeer() {
+    var searchBar = document.querySelector("#searchBar").value;
+    var url = "http://beermapping.com/webservice/loccity/f82c35d11ba2db8be445456f47487b12/" + searchBar + "&s=json"
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+};
