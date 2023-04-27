d23359e06ffd323c15fe714243bbbcae7e113b33
JDunc <jjsduncan@gmail.com>
2023-04-27 19:02:54 -0400

    add getBeer API call function

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,3 @@
-// $(document).ready(function(){ 
-//     console.log("hello")
-
-//     const apikey = "fa8a706fcf011be6955a40353b9e2226"
-
-    
-//     function search (alchol){
-//         console.log("clicked")
-//         fetch(
-//             `https://beermapping.com/api/request/=${alcohol}&units=imperial&appid=${apikey}`
-//         ).then(function(response){
-//             return response.json()
-//         }).then(function(data){
-//     console.log(data)
-//     // var beer = 
-
 
 console.log("howdy");
 
@@ -21,25 +5,40 @@ console.log("howdy");
 
 document.querySelector("#applyButton").addEventListener("click", getCity);
 
-function getCity () {
+// document.querySelector("#applyButton").addEventListener("click", getBeer);
+
+function getCity() {
     var searchBar = document.querySelector("#searchBar").value;
     var url = "https://developer.nps.gov/api/v1/parks?q=" + searchBar + "&api_key=8fzgFBy23bOctVVOvssIxHSKu8vDZPKnUKcTNKfM"
-        fetch(url)
+    fetch(url)
         .then(function (response) {
             return response.json()
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
-        })       
+        })
 };
 
 const resetButton = document.querySelector("#resetButton");
-resetButton.addEventListener("click", function(){
+resetButton.addEventListener("click", function () {
     const inputField = document.querySelector(".input");
     inputField.value = "";
 })
 
 
 
+// Beer Mapping API Key: f82c35d11ba2db8be445456f47487b12
 
-console.log("test");
+
+
+// function getBeer() {
+//     var searchBar = document.querySelector("#searchBar").value;
+//     var url = "http://beermapping.com/webservice/locquery/f82c35d11ba2db8be445456f47487b12/" + searchBar + "&s=json"
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+// };
