1d472481ed061d4be4a72547fac1ef4e54b331ae
otterslacks <antonelloluke@gmail.com>
2023-05-01 11:28:55 -0400

    added footer to HTML and weather API working code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // $(document).ready(function(){ 
 //     console.log("hello")
 
-//     const apikey = "fa8a706fcf011be6955a40353b9e2226"
+//     var apikey = "fa8a706fcf011be6955a40353b9e2226"
 
     
 //     function search (alchol){
@@ -33,8 +33,48 @@ function getCity () {
         })       
 };
 
+// variable declarations
+var apiKey = "69550612ac9d43328d7234409232504";
+var weatherCard = document.getElementById("weatherCard");
+var searchBar = document.getElementById("searchBar");
+var applyButton = document.getElementById("applyButton");
+
+// search form
+applyButton.addEventListener("click", function() {
+    var city = searchBar.value;
+
+//   Create API fetch
+    fetch("https://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=" + city + "&days=3")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var forecast = data.forecast.forecastday;
+  
+        var cardContent = city + "Weather Forecast";
+//   Modify
+        for ( var i= 0; i < forecast.length; i++) {
+          var date = forecast[i].date;
+          var weatherIcon = forecast[i].day.condition.icon;
+          var weatherDescription = forecast[i].day.condition.text;
+          var highTemp = forecast[i].day.maxtemp_f;
+          var lowTemp = forecast[i].day.mintemp_f;
+//  Append 
+          cardContent += "<div class='forecastDay'>" +
+            "<p><strong>" + date + "</strong></p>" +
+            "<img src='" + weatherIcon + "' alt='" + weatherDescription + "'/>" +
+            "<p><strong>Weather:</strong> " + weatherDescription + "</p>" +
+            "<p><strong>High Temp:</strong> " + highTemp + "°F</p>" +
+            "<p><strong>Low Temp:</strong> " + lowTemp + "°F</p>" +
+            "</div>";
+        }
+  
+        weatherCard.innerHTML = cardContent;
+      })
+    //   .catch(function(error) {
+    //     console.error(error);
+    //     weatherCard.innerHTML = "<h2>Error loading weather forecast</h2>";
+    //   });
+  });
 
 
-
-
-console.log("test");
